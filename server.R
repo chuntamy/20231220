f273377ceadc73058d817d6df7700a4f07d5630b
LE404A01\uer <chuntamy213@gmail.com>
2023-12-20 16:55:31 +0800

    linear regression

--- server.R
+++ server.R
@@ -2,18 +2,47 @@ library(shiny)
 
 shinyServer(function(input, output) {
   
-  output$summary = renderPrint({
-    if (is.null(input$files)==TRUE) {return("You have to up load your data!!!")} else {
-      dat = read.table(input$files$datapath, header = TRUE)
-      summary(dat)                     
-    }        
+  DATA <- reactive({
+    if (is.null(input$files)) {return()} else {
+      dat <- read.table(input$files$datapath,header=T)
+      return(dat) 
+    }
   })
   
-  output$view = renderTable({
-    if (is.null(input$files)==TRUE) {return()} else {
-      dat = read.table(input$files$datapath, header = TRUE)
-      head(dat, input$n)                     
-    }        
+  output$choose_columns1 <- renderUI({  #這裡是關鍵
+    dat = DATA()
+    if (is.null(dat)) {return()} else {
+      colnames <- colnames(dat)
+      selectInput("Y", h4("Choose a dependence variable:"), choices = colnames)
+    }
+  })
+  
+  output$choose_columns2 <- renderUI({  #這裡是關鍵
+    dat = DATA()
+    if (is.null(dat)|is.null(input$Y)) {return()} else {
+      colnames <- colnames(dat)
+      selectInput("X", h4("Choose a independence variable:"), choices = colnames[which(colnames!=input$Y)])
+    }
+  })
+  
+  output$summary <- renderPrint({
+    dat = DATA()
+    if (is.null(dat)|is.null(input$Y)|is.null(input$X)) {return()} else {
+      X <- dat[,input$X]  #這裡是關鍵
+      Y <- dat[,input$Y]  #這裡是關鍵
+      Result=lm(Y~X)
+      return(summary(Result))
+    }  
+  })
+  
+  output$plot <- renderPlot({
+    dat = DATA()
+    if (is.null(dat)|is.null(input$Y)|is.null(input$X)) {return()} else {
+      X <- dat[,input$X]  #這裡是關鍵
+      Y <- dat[,input$Y]  #這裡是關鍵
+      plot(X,Y,pch=19)
+      abline(lm(Y~X),col="black")   
+    }  
   })
   
 })
--- ui.R
+++ ui.R
@@ -2,17 +2,18 @@ library(shiny)
 
 shinyUI(pageWithSidebar(
   
-  headerPanel("Summarize your data"), 
+  headerPanel("Linear regression for two continuous variables."), 
   
   sidebarPanel(
     fileInput(inputId="files", label=h4("Upload your data file:"), multiple=FALSE, accept="text/plain"),
     helpText("Note: you only can upload the .txt file."),
-    sliderInput("n", h4("Select the observed rows:"), min=5, max=20, value=10)
+    uiOutput("choose_columns1"),   #這裡是關鍵
+    uiOutput("choose_columns2")    #這裡是關鍵
   ),
   
   mainPanel(
-    verbatimTextOutput("summary"),  
-    tableOutput("view")  
+    verbatimTextOutput("summary"),
+    plotOutput("plot",width = "500px", height = "500px")
   )  
   
 ))
